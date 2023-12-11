62c63cd06115589f07e837b4d5253f874efb3e5f
Hari Kishore <harikishore@gmail.com>
2023-12-10 23:55:46 -0500

    barebones layout

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+function Footer () {
+    return (
+        <footer>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+function Header () {
+    return (
+        <header>
+            <img src="../assets/Logo.svg" alt="Logo"></img>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
